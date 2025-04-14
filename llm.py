3099d6efc50bb2c54ee8a1b00f89811bca874916
ZaynIkhlaq <zaynikhlaq@gmail.com>
2025-04-15 02:46:09 +0500

    fixed llm api

--- llm.py
+++ llm.py
@@ -10,4 +10,4 @@ if not os.environ.get("GROQ_API_KEY"):
     raise ValueError("Please set GROQ_API_KEY in your .env file")
 
 # Initialize the LLM
-llm = init_chat_model("llama3-70b-8192", model_provider="groq")
+llm = init_chat_model("meta-llama/llama-4-maverick-17b-128e-instruct", model_provider="groq")
